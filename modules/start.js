ba5eb5824f9cfa44587500222b854b204384dd56
Uvindu Bro <79355885+UvinduBro@users.noreply.github.com>
2021-05-04 15:59:38 +0530

    Update start.js

--- modules/start.js
+++ modules/start.js
@@ -10,9 +10,9 @@ async function get(battery, phn_info) {
     }
 
     return ({
-        msg: `*Whatsbot* _(1.5.0)_\n\nThis chat is Powered By *Whatsbot*\n\n*Battery:* ${batttxt}\n*Device:* ${phn_info.device_manufacturer} ${phn_info.device_model}\n*WA Version:* ${phn_info.wa_version}\n*Pmpermit:* ${config.pmpermit_enabled}\n*Mutetime:* ${config.pmpermit_mutetime/60} Minutes\n\n*Official Repository Url 👇*\n` + "```https://github.com/UvinduBro/WhatsBot```",
+        msg: `*Whatsbot* _(1.5.0)_\n\nThis chat is Powered By *Uvindu Bro*\n\n*🔋 Battery:* ${batttxt}\n*📱 Device:* ${phn_info.device_manufacturer} ${phn_info.device_model}\n*🚀 WA Version:* ${phn_info.wa_version}\n*📥 PM permit:* ${config.pmpermit_enabled}\n*🔇 Mutetime:* ${config.pmpermit_mutetime/60} Minutes\n\n*Uvindu Bro Website*\n` + "```https://www.uvindubro.tk```",
         mimetype: "image/jpeg",
-        data: Buffer.from(((await axios.get('https://telegra.ph/file/ecbc27f276890bf2f65a2.jpg', { responseType: 'arraybuffer' })).data)).toString('base64'),
+        data: Buffer.from(((await axios.get('https://telegra.ph/file/62c432d33ca058a5caab5.jpg', { responseType: 'arraybuffer' })).data)).toString('base64'),
         filename: "start.jpg"
     })
 }
